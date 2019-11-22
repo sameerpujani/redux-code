9324e868a986811fe09cca7d2fd38e53ecc19c64
Sameer Pujani <pujanisameer@gmail.com>
2019-11-22 23:39:54 +0530

    finding

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,16 @@ import ReactDOM from 'react-dom';
 
 // Create a react Component
 const App = () => {
-    return <div>Hi There!</div>;
+    const buttonText = {text: 'Click me'};
+    const labelText = 'Enter name:';
+    
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelText}</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        </div>
+    )
 };
 
 // Take the react Component and show it on the screen
