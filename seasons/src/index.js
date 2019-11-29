477794a75671b850ddfb62f934ac126d1556f812
sameerpujani <sameerpujanigndpc@gmail.com>
2019-11-29 23:38:27 +0530

    avoid conditions in render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -23,8 +23,7 @@ class App extends React.Component {
         );
     }
 
-    // React says we have to define render!!
-    render() {
+    renderContent() {
         if( this.state.errorMessage && !this.state.lat ) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -33,6 +32,15 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept invitation request" />;
+    }
+
+    // React says we have to define render!!
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
