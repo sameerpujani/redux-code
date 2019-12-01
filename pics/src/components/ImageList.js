ee1044d88d9ee9b78cd615a45483d6edd07652cc
sameerpujani <sameerpujanigndpc@gmail.com>
2019-12-01 17:19:02 +0530

    show images list

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    const images = props.images.map(({id, alt_description, urls}) => (
+        <img key={id} alt={alt_description} src={urls.regular} />
+    ))
+
+    return (
+        <div className="image-list">{images}</div>
+    )
+}
+
+export default ImageList;
