084e31c832c141d8dbf3e571121c386e42c241a3
sameerpujani <sameerpujanigndpc@gmail.com>
2020-03-25 21:44:06 +0530

    basic react app using redux lib

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        {title: 'No Scrubs', duration: '04:05'},
+        {title: 'Macarena', duration: '02:30'},
+        {title: 'All Star', duration: '03:15'},
+        {title: 'I want it that Way', duration: '01:45'}
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
